09fb159588b71abd98b53a2a5ccadff3586782f2
Michal Franczak <rahnidos@stricte.net>
2020-09-16 20:14:18 +0200

    first functions

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from dbConnector import dbConnector
 from telegram.ext import CommandHandler
 from telegram.ext import Updater
+#from staszek import staszek
 import logging
 import sys
 
@@ -9,9 +10,22 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 db = dbConnector.Instance()
 
+def findUserName(update, context):
+    tusername=update.message.from_user.first_name
+    if (update.message.from_user.last_name): tusername=update.message.from_user.last_name
+    if (update.message.from_user.username): tusername=update.message.from_user.username
+    return tusername
+def answerTxt(update, context, ans):
+    ans='@'+findUserName(update, context)+': '+ans
+    context.bot.sendMessage(chat_id=update.effective_chat.id, text=ans)
+#@#TODO answer photo
+
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Jestem Bot. Staszek Bot.")
+    answerTxt(update, context, 'Jestem Bot. Staszek Bot.')
     #@#TODO Komunikat startowy niech bierze też z bazy jako parametr
+
+def question(update, context):
+    answerTxt(update, context, 'tu będzie odpowiedź')
 
 def main():
     btoken=db.getParam('token')
@@ -20,8 +34,9 @@ def main():
         sys.exit(1)
     updater = Updater(token=btoken, use_context=True)
     dispatcher = updater.dispatcher
-
-    dispatcher.add_handler(CommandHandler("start", start))
+    comlist=db.getCommands()
+    for com in comlist:
+        dispatcher.add_handler(CommandHandler(com[0], eval(com[1])))
 
     updater.start_polling()
     updater.idle()
--- dbConnector.py
+++ dbConnector.py
@@ -1,4 +1,5 @@
 from singleton import Singleton
+#from staszek imort staszek
 import sqlite3
 
 @Singleton
@@ -20,6 +21,13 @@ class dbConnector(object):
         if r: return r[0]
         else: return False
 
+    def select_dict(self, q):
+        cur = self.__conn.cursor()
+        cur.execute(q)
+        r = cur.fetchall()
+        if r: return r
+        else: return False
+
     def execute(self, q):
         #@# TODO: błędy wyrzucić do loggera
         cur = self.__conn.cursor()
@@ -34,8 +42,7 @@ class dbConnector(object):
     def getParam(self, key):
         q = "select value from settings where key='%s'" % (key)
         ret = self.select_single(q)
-        if ret != False: return ret
-        else: return False
+        return ret
     def setParam(self, key, value):
         q = "insert into settings (key,value) values('%s','%s')" % (key,value)
         return self.execute(q)
@@ -45,3 +52,12 @@ class dbConnector(object):
     def updateParam(self, key, value):
         q = "update settings set value = '%s' where key = '%s'" % (value,key)
         return self.execute(q)
+
+    def getCommand(self, alias):
+        q = "slelect value from commands where alias='%s'" % (alias)
+        ret = self.select_single(q)
+        return ret
+    def getCommands(self):
+        q = "select alias, real, type from commands"
+        ret = self.select_dict(q)
+        return ret
