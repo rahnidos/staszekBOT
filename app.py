a82a704868bfd59ee36c32748e3e11be60ea0e50
rahn <rahnidos@gmail.com>
2024-05-30 18:43:57 +0200

    openai connection

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from registry import registry
 import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from openai import OpenAI
 
 R = registry.Instance()
 D = dbConnector.Instance()
@@ -11,28 +12,58 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.WARN
 )
+client=OpenAI(
+    api_key=R.cfg['openai']
+)
+
+
+messages = [ {"role": "system", "content": "You are a intelligent assistant."} ]
+
+
 def prepareCommandsHandlers():
-    rCommList=D.select_list('select alias from commands where type=1')
+    rCommList=D.select_list('select real from commands where type=1')
     for command in rCommList:
-        start_handler=CommandHandler(command[0],eval(command[0]))
+        application.add_handler(CommandHandler(command[0],eval(command[0])))
+
+def aksGepetto(txt):
+    try:
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role":"user","content":txt}
+            ]
+        )
+        return completion.choices[0].message['content']
+    except Exception as e:
+        print(e)
+        return "mam problemy z pamięcią"
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Hola soy Staszek")
 
 async def question(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Question Handler")
+    answer=aksGepetto(" ".join(context.args))
+    messages.append({"role":"user", "content":answer})
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
 
+async def roll(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Roll Handler")
+
+async def randMember(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="randMemebr Handler")
+
+async def rollLocation(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="roll Location")
 
 
 if __name__ == '__main__':
 
     
-    #application = ApplicationBuilder().token(R.token).build()
+    application = ApplicationBuilder().token(R.cfg['token']).build()
     
-    #start_handler = CommandHandler('start', start)
     
     prepareCommandsHandlers()
-    #application.add_handler(start_handler)
     
-    #application.run_polling()
+    application.run_polling()
 
--- registry.py
+++ registry.py
@@ -4,12 +4,16 @@ import os
 
 @Singleton
 class registry(object):
+    cfg={}
+
     def __init__(self):
         self.sbhome=os.environ.get('STASZEKHOME')
         self.sbdb=os.path.join(os.environ.get('STASZEKHOME'),'staszek.db')
         D = dbConnector.Instance()
-        self.token=D.select_single("select value from settings where key='token'")
-        self.ownerId=D.select_single("select value from settings where key='owner'")
+        settings=D.select_list("select key,value from settings")
+        for setting in settings:
+            self.cfg[setting[0]]=setting[1]
+                
         pass
 
     def __str__(self):
